a83691eef5cf33077f439dc873233e295d499b4f
Mukeswar <vasanamukeswar@gmail.com>
2025-08-07 13:46:43 -0700

    nothing api change

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -1,8 +1,8 @@
 
 
 
-     // export const apiUrl = process.env.REACT_APP_API_URL;
-     export const apiUrl = "https://16.171.197.191";
+     export const apiUrl = process.env.REACT_APP_API_URL;
+     // export const apiUrl = "https://16.171.197.191";
 
 
 const ApplicantAPIService = {
