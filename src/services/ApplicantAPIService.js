fe7c7b7f88114ce5b2938ea83c97d1d4f15ab1d1
Mukeswar <vasanamukeswar@gmail.com>
2025-08-07 10:22:29 -0700

    updated

--- src/services/ApplicantAPIService.js
+++ src/services/ApplicantAPIService.js
@@ -1,8 +1,8 @@
 
 
 
-     // export const apiUrl = process.env.REACT_APP_API_URL;
-     export const apiUrl = "http://192.168.200.105:8081";
+     export const apiUrl = process.env.REACT_APP_API_URL;
+     // export const apiUrl = "http://192.168.200.105:8081";
 
 
 const ApplicantAPIService = {
